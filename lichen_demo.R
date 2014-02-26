d86547917d68a95f06c288b7e49ea6d0d3ce8e86
csteenbock <christopher.steenbock@gmail.com>
2014-02-26 10:42:01 -0700

    for loop solution

--- lichen_demo.R
+++ lichen_demo.R
@@ -1,53 +1,107 @@
 ## C Steenbock
-## 30 January 2014
+## 21 February 2014
 ## From data to a demographic model
 
-library(ggplot2)
+rm(list=ls())
+library(dplyr)
 
 dat <- read.csv("lichen_demo.csv")
-t0 <- dat$sqrtszt
-t1 <- dat$sqrtszt1
+dat.size <- as.data.frame(dat[, 5:6])
+colnames(dat.size) <- c("sqrtszt"="t0", "sqrtszt1"="t1") # rename column header
+size <- arrange(dat.size, t0) # arrange initial column in ascending order
 
-par(mfrow = c(1,2)) # histogram of indv size at both times
-hist(t0)    # first census
-hist(t1)    # second census
+n.bin <- 5  # specify number of classes
 
-surv <- rep(NA,length(t1)) # Initialize vector to hold 0/1's, survivorship
-# Use for loop to assign zeros and ones to surv vector based on 2nd census
-for(i in 1:length(t1)){
-    if (t1[i] != 0){
-        surv[i] <- 1
-    } else {
-        surv[i] <- 0
-        }
+# initialize storage
+n <- rep(NA, n.bin)                         # count of indvs per bin
+surv <- rep(NA, n.bin)
+grow <- matrix(NA, n.bin, n.bin)            # store growth probabilites for each size class
+
+vec.bin <- rep(NA, n.bin)                   # init. container for bin breaks
+vec.bin[1] <- 0 
+vec.bin[n.bin+1] <- max(size[,1])           # the last bin needs an upper limit specified
+
+# fill vector with evenly spaced bin breaks
+for(i in 1:(n.bin-1)){
+    bin.width <- (max(size[,1]) - min(size[,1])) / n.bin    # find bin width
+    vec.bin[i+1] <- vec.bin[i] + bin.width                  # assign values to bin break vector
 }
 
-size.surv <- as.data.frame(cbind(t0, surv)) # append size vector and surv vector
-
-# plot surviorship v. size
-surv.plot <- ggplot(size.surv, aes(t0, surv)) + 
-    geom_point(position=position_jitter(w=0.03, h=0.03), alpha=0.2, size=5)
-surv.plot
-
-# bin size data into classes
-size <- sort(dat$sqrtszt)
-bin.size <- split(size, ceiling(seq_along(size)/136))
-med.size <- lapply(bin.size, median)
-
-# logistic regression model of surv as fx of size
-mod1 <- glm(surv~t0, family=binomial, data=size.surv)
-mod1.plot <- surv.plot + geom_smooth(method="glm", family="binomial")
-
-# function takes median values from each size class and uses it as the ind. var
-# in the equation from the above fitted logisic regression model.  plogis is
-# necessary to transform output from log-odds to probabilites
-mod1.fun <- function(x){
-    pr.surv <- plogis(mod1$coef[1]+mod1$coef[2]*x)
-    return(pr.surv)
+# bin, survival, growth
+for(i in 1:(length(vec.bin)-1)){
+    bounds <- c(vec.bin[i], vec.bin[i+1])                           # set limits for subset according to bin breaks
+    subset <- size[size[,1] > bounds[1] & size[,1] < bounds[2],]    # subset data according to bounds
+    n[i] <- length(subset[,1])                                      # store number of inviduals in this bin for future ref
+    surv[i] <- sum(subset[,2] != 0) / length(subset[,2])            # calculate survivorship for this class, note TRUE = 1, which is a possible return of the logical expression !=
+    histo <- hist(subset$t1, breaks = vec.bin, plot = FALSE)        # store hist as object, to access counts per bin
+    grow[,i] <- histo$counts/length(subset[,1])                     # $counts returns the number of individuals of a certain size class                                                                   # at t1 from the bin set at t0, that is growth or shrinkage or neither   
 }
-# apply above function to median values; returns a list of probs for each class
-surv.est <- lapply(med.size, mod1.fun)
+
+n
+surv
+grow
+
+M <- matrix(NA, n.bin, n.bin)
+
+for(i in 1:length(surv)){
+    M[,i] <- surv[i] * grow[,i]  
+}
 
 
+
+
+
+
+# ###################
+# #### An attempt to parameterize the model by logistic regression, and class based vital rates
+# library(ggplot2)
+# 
+# dat <- read.csv("lichen_demo.csv")
+# t0 <- dat$sqrtszt
+# t1 <- dat$sqrtszt1
+# 
+# par(mfrow = c(1,2)) # histogram of indv size at both times
+# hist(t0)    # first census
+# hist(t1)    # second census
+# 
+# surv <- rep(NA,length(t1)) # Initialize vector to hold 0/1's, survivorship
+# # Use for loop to assign zeros and ones to surv vector based on 2nd census
+# for(i in 1:length(t1)){
+#     if (t1[i] != 0){
+#         surv[i] <- 1
+#     } else {
+#         surv[i] <- 0
+#         }
+# }
+# 
+# size.surv <- as.data.frame(cbind(t0, surv)) # append size vector and surv vector
+# 
+# # plot surviorship v. size
+# surv.plot <- ggplot(size.surv, aes(t0, surv)) + 
+#     geom_point(position=position_jitter(w=0.03, h=0.03), alpha=0.2, size=5)
+# surv.plot
+# 
+# # bin size data into classes
+# size <- sort(dat$sqrtszt)
+# bin.size <- split(size, ceiling(seq_along(size)/203))
+# med.size <- lapply(bin.size, median)
+# 
+# # logistic regression model of surv as fx of size
+# mod1 <- glm(surv~t0, family=binomial, data=size.surv)
+# (mod1.plot <- surv.plot + geom_smooth(method="glm", family="binomial"))
+# 
+# # function takes median values from each size class and uses it as the ind. var
+# # in the equation from the above fitted logisic regression model.  plogis is
+# # necessary to transform output from log-odds to probabilites
+# 
+# mod1.fun <- function(x){
+#     pr.surv <- plogis(mod1$coef[1]+mod1$coef[2]*x)
+#     return(pr.surv)
+# }
+# # apply above function to median values; returns a list of probs for each class
+# vital.surv <- lapply(med.size, mod1.fun)
+# 
+# ################
+# ################
 
 
