174d51e70e249129be2d06c4cacaa5582c780481
csteenbock <christopher.steenbock@gmail.com>
2014-03-11 15:14:25 -0600

    omg that took forever

--- lichen_demo.R
+++ lichen_demo.R
@@ -32,7 +32,6 @@ bin.breaks <- function(user_spec = NULL, bins){
         }
         return(vec.bin) 
     } else {
-        spec <- user_spec
         n.bin <- bins
         vec.bin <- rep(NA, n.bin)                               # init. container for bin breaks
         # assign user specified break points to vec.bin
@@ -48,20 +47,22 @@ bin.breaks <- function(user_spec = NULL, bins){
         return(vec.bin)    
     }
 }
-### xxxx -----------------------------
+### Estimate vital rates, populate matrix -----------------------------
 
-# generate bin break vector using bin.breaks(); req arg is "bins"
-vec.bin <- bin.breaks()
-
-# initialize storage
-n <- rep(NA, n.bin)                         # count of indvs per bin
-median <- rep(NA, n.bin)                    # median size per bin for F
-surv <- rep(NA, n.bin)
-grow <- matrix(NA, n.bin, n.bin)            # store growth probabilites for each size class
+bin.num <- seq(from = 4, to = 25, by = 1)
 lambdas <- rep(NA, length(bin.num))
     
-
+for(i in 1:length(bin.num)){
     
+    vec.bin <- bin.breaks(user_spec = c(0, sqrt(0.05)), bins = bin.num[i])
+    
+    ## Initialize storage
+    n.bin <- length(vec.bin)-1                  # this is a workaround
+    n <- rep(NA, n.bin)                         # count of indvs per bin
+    median <- rep(NA, n.bin)                    # median size per bin for F
+    surv <- rep(NA, n.bin)                      # survivorship for each class
+    grow <- matrix(NA, n.bin, n.bin)            # store growth probabilites for each class
+
     # bin, survival, growth
     for(i in 1:(length(vec.bin)-1)){
         bounds <- c(vec.bin[i], vec.bin[i+1])                           # set limits for subset according to bin breaks
@@ -71,13 +72,13 @@ lambdas <- rep(NA, length(bin.num))
         surv[i] <- sum(subset[,2] != 0) / length(subset[,2])            # calculate survivorship for this class, note TRUE = 1, which is a possible return of the logical expression !=
         histo <- hist(subset$t1, breaks = vec.bin, plot = FALSE)        # store hist as object, to access counts per bin
         grow[,i] <- histo$counts/length(subset[,1])                     # $counts returns the number of individuals of a certain size class                                                                   
-        # at t1 from the bin set at t0, that is growth or shrinkage or neither   
     }
     
     M <- matrix(NA, n.bin, n.bin)   # initiate projection matrix
+    
+    # populate projection martix, specific to lichen data set
     r <- 0.185                      # MLE for r from Shriver (2012)
     
-    # populate projection martix
     for(i in 1:length(surv)){
         M[,i] <- surv[i] * grow[,i]
         M[1,i] <- (r * median[i]) + (surv[i] * grow[1,i]) # how does growth fit into M[1,i]?
@@ -87,9 +88,8 @@ lambdas <- rep(NA, length(bin.num))
 
 }
 
-
-
-
+lambdas
+plot(lambdas, type = 'l')
 
 
 
